6250b0c67a51891f77d418674ed5bdfbbcd046b8
shubham09996 <shubhamgupta0996@gmail.com>
2025-10-18 11:20:30 +0530

    Fixing minor errors

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -28,7 +28,7 @@ const authUser = asyncHandler(async (req, res) => {
     res.cookie('jwt', token, {
       httpOnly: true,
       secure: process.env.NODE_ENV !== 'development',
-      sameSite: 'strict',
+      sameSite: 'none', // Changed from 'strict' to 'none' for cross-site cookies
       maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
     });
 
@@ -129,7 +129,7 @@ const registerUser = asyncHandler(async (req, res) => {
     res.cookie('jwt', token, {
       httpOnly: true,
       secure: process.env.NODE_ENV !== 'development',
-      sameSite: 'strict',
+      sameSite: 'none', // Changed from 'strict' to 'none' for cross-site cookies
       maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
     });
 
